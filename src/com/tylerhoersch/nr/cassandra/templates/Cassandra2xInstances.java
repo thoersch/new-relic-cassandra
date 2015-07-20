21531d353b25ab0a725db0bb8b75062851e9171d
Tyler Hoersch <thoersch@gmail.com>
2015-07-20 11:25:34 -0500

    added logging to instance poking

--- src/com/tylerhoersch/nr/cassandra/templates/Cassandra2xInstances.java
+++ src/com/tylerhoersch/nr/cassandra/templates/Cassandra2xInstances.java
@@ -1,5 +1,6 @@
 package com.tylerhoersch.nr.cassandra.templates;
 
+import com.newrelic.metrics.publish.util.Logger;
 import com.tylerhoersch.nr.cassandra.JMXRunner;
 import com.tylerhoersch.nr.cassandra.JMXTemplate;
 
@@ -10,6 +11,9 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Cassandra2xInstances implements JMXTemplate<List<String>> {
+
+    private static final Logger logger = Logger.getLogger(Cassandra2xInstances.class);
+
     @SuppressWarnings("unchecked")
     @Override
     public List<String> execute(MBeanServerConnection connection, JMXRunner jmxRunner) throws Exception {
@@ -21,7 +25,8 @@ public class Cassandra2xInstances implements JMXTemplate<List<String>> {
             return instances;
         }
 
-        instances = (List<String>) scores.keySet().stream().map(s -> s.toString().substring(s.toString().indexOf("/")+1, s.toString().length())).collect(Collectors.toList());
+        logger.debug("Scores:" + scores.keySet().toString());
+        instances = (List<String>) scores.keySet().stream().map(s -> s.toString().substring(s.toString().indexOf("/") + 1, s.toString().length())).collect(Collectors.toList());
 
         return instances;
     }
