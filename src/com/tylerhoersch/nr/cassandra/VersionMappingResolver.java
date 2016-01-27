348a9bd4184e7d7d8e7b6c3691b716893b50c90b
Tim Fendt <tim.fendt@virginpulse.com>
2016-01-27 16:54:02 -0600

    Added missing 3x mappings to version resolver.

--- src/com/tylerhoersch/nr/cassandra/VersionMappingResolver.java
+++ src/com/tylerhoersch/nr/cassandra/VersionMappingResolver.java
@@ -1,6 +1,7 @@
 package com.tylerhoersch.nr.cassandra;
 
 import com.tylerhoersch.nr.cassandra.templates.Cassandra2xMappings;
+import com.tylerhoersch.nr.cassandra.templates.Cassandra3xMappings;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -9,6 +10,7 @@ public class VersionMappingResolver {
 
     private final Map<Integer, AttributeJMXRequestMapper> mappers = new HashMap<Integer, AttributeJMXRequestMapper>() {{
         put(2, new Cassandra2xMappings());
+        put(3, new Cassandra3xMappings());
     }};
 
     public AttributeJMXRequestMapper resolve(int version) {
